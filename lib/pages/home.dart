48adc1e31ca942a6a6e0aa9f79f27a528dc54bf5
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-23 16:50:44 -0300

    ✨ Adicionado estrutura pag Home

---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:myvan_flutter/components/bottom_menu.dart';
+import 'package:myvan_flutter/components/custom_app_bar.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Home'),
+      bottomNavigationBar: BottomMenu(),
+    );
+  }
+}
