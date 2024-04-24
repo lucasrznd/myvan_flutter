c55da4fb0b8bd6693e3bfd1227a6e64cce98489a
João Revoredo <augustobatista48@gmail.com>
2024-04-24 20:32:49 -0300

    Botão Chamada

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:myvan_flutter/components/bottom_menu.dart';
 import 'package:myvan_flutter/components/custom_app_bar.dart';
+import 'package:myvan_flutter/components/botao_chamada.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -12,8 +13,11 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: CustomAppBar(title: 'Home'),
+      body: Container(
+        child: CardExample(),
+      ),
       bottomNavigationBar: BottomMenu(),
     );
   }
