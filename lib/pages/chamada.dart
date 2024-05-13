8b3e3e5b07e4e51d772a1f9df39ef067f7b57f8b
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-13 11:10:23 -0300

    ✨ Criada página de chamada

---
+++ lib/pages/chamada.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChamadaPage extends StatefulWidget {
+  const ChamadaPage({super.key});
+
+  @override
+  State<ChamadaPage> createState() => _ChamadaPageState();
+}
+
+class _ChamadaPageState extends State<ChamadaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
