78841b37d9c17bcf88f006eff0e9287c8007b777
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-15 16:47:43 -0300

    ♻️ Removido redundancias de metodos

--- lib/services/chamada_service.dart
+++ lib/services/chamada_service.dart
@@ -3,41 +3,36 @@ import 'package:myvan_flutter/models/enums/tipo_viagem.dart';
 import 'package:myvan_flutter/models/passageiro.dart';
 import 'package:myvan_flutter/models/viagem.dart';
 import 'package:myvan_flutter/repositories/chamada_repository.dart';
-import 'package:myvan_flutter/repositories/passageiro_repository.dart';
 import 'package:myvan_flutter/repositories/viagem_repository.dart';
+import 'package:myvan_flutter/services/passageiro_service.dart';
 
 class ChamadaService {
   final ChamadaRepository _repository = ChamadaRepository();
+  final PassageiroService _passageiroService = PassageiroService();
 
   Future<List<ChamadaPassageiro>> selectAllHoje(TipoViagem tipoViagem) async {
-    List<ChamadaPassageiro> chamadas =
-        await _repository.selectAllHoje(tipoViagem.descricao);
-    return chamadas;
+    return await _repository.selectAllHoje(tipoViagem.descricao);
   }
 
   Future<List<ChamadaPassageiro>> selectAll() {
-    return _repository.selectAll(); 
+    return _repository.selectAll();
   }
 
   Future<List<ChamadaPassageiro>> listarPresentes(TipoViagem tipoViagem) async {
-    List<ChamadaPassageiro> chamadas =
-        await _repository.listarPresentesHoje(tipoViagem.descricao);
-    return chamadas;
+    return await _repository.listarPresentesHoje(tipoViagem.descricao);
   }
 
   Future<List<ChamadaPassageiro>> listarAusentes(TipoViagem tipoViagem) async {
-    List<ChamadaPassageiro> chamadas =
-        await _repository.listarAusentesHoje(tipoViagem.descricao);
-    return chamadas;
+    return await _repository.listarAusentesHoje(tipoViagem.descricao);
   }
 
   void criarChamadaIda() async {
     ViagemRepository viagemRepository = ViagemRepository();
-    PassageiroRepository passageiroRepository = PassageiroRepository();
+
     Viagem viagem =
         await viagemRepository.obterUltimaViagem(TipoViagem.ida.descricao);
 
-    List<Passageiro> passageiros = await passageiroRepository.selectAll();
+    List<Passageiro> passageiros = await _passageiroService.selectAll();
 
     for (var passageiro in passageiros) {
       ChamadaPassageiro chamada = ChamadaPassageiro(
@@ -48,11 +43,11 @@ class ChamadaService {
 
   void criarChamadaVolta() async {
     ViagemRepository viagemRepository = ViagemRepository();
-    PassageiroRepository passageiroRepository = PassageiroRepository();
+
     Viagem viagem =
         await viagemRepository.obterUltimaViagem(TipoViagem.volta.descricao);
 
-    List<Passageiro> passageiros = await passageiroRepository.selectAll();
+    List<Passageiro> passageiros = await _passageiroService.selectAll();
 
     for (var passageiro in passageiros) {
       ChamadaPassageiro chamada = ChamadaPassageiro(
